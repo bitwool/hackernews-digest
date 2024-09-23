dedbff910ffe048b1ec13da5731b91686902e562
bitwool <wuchuanming1990@gmail.com>
2024-09-23 17:09:41 +0800

    更新ArticleList的property属性

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import ArticleList from './components/ArticleList';
-import Sidebar from './components/Sidebar';
+import ArticleList from "./components/ArticleList";
+import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
     <div className="flex h-screen">
       <Sidebar />
       <main className="flex-1 p-4 overflow-y-auto">
-        <ArticleList date="2023-09-23" />
+        <ArticleList initialDate="2023-09-23" />
       </main>
     </div>
   );
